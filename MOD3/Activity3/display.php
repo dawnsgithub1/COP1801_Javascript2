447c3debfa7186fc10aee0cf85ad66b70582a580
dawnsgithub1 <dawn.scott@smail.rasmussen.edu>
2019-10-27 18:08:06 -0500

    MOD3 Activity
    A visual representation/skeleton/blueprint.
    The wireframe should NOT attempt to show the final visual design, such as fonts and colors.
    It must include navigation components.
    It must include thumbnail components.
    It may include headers, footers, content areas, sidebars (you may be creative here).

---
+++ MOD3/Activity3/display.php
@@ -0,0 +1,21 @@
+<?php
+include 'mod3_config.php';
+include 'mod3_opendb.php';
+
+$sql= "SELECT FirstName, LastName, EmailAddress FROM contact LIMIT 10";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+      echo "FirstName: " . $row["FirstName"]. "<br>";
+      echo "LastName: " . $row["LastName"]. "<br>";
+      echo "EmailAddress: " . $row["EmailAddress"]. "<br><hr>";
+  }
+} else {
+  echo "0 results";
+}
+
+mysqli_close($conn);
+
+?>
