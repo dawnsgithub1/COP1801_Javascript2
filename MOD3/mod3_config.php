8fa6cd8e5f5447ee231b36360a2ec9f0c1404b4b
dawnsgithub1 <dawn.scott@smail.rasmussen.edu>
2019-10-27 21:05:50 -0500

    MOD3Project
    Create a sample database and table using phpMyAdmin in your Microsoft Azure account.
    Populate the database with data that is related to your website. This must include at least 4 columns, including a primary key.
    Construct a simple SELECT query with 3 or more columns.
    Construct a web page with responsive styling that displays the results of a query.

--- MOD3/mod3_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// This is an example of config.php
-$dbhost = 'localhost';
-$dbuser = 'fmwadvmAdmin';
-$dbpass = 'fmwadvmPassW1';
-$dbname = 'mysampledb';
-?>
--- MOD3/mod3_opendb.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Create connection
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli.connect_error());
-}
-?>
